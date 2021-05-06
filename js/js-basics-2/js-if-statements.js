3edfb810801f1961ad06df4b9ade56b85a5e6b43
KimberN <kimberly.k.noyes@gmail.com.com>
2021-05-06 07:38:33 -0600

    commit

---
+++ js/js-basics-2/js-if-statements.js
@@ -0,0 +1,6 @@
+// Define a variable called ‘hoursofSleep’. Set the valueof the variable to 8
+var hoursofSleep = 8;
+if (hoursofSleep <= 6){
+   console.log('I am Groggy')
+}
+else console.log('I feel fantasic!')
---
+++ js/js-fiddle/js-var.js
@@ -0,0 +1,41 @@
+/*console.log('hello world');
+var x=1
+console.log('x');
+var y= "cat";
+console.log(y);
+var x=5;
+var y=3;
+var sum=x+y
+console.log(sum);*/
+var x= 3
+console.log(x);
+var s='gosh';
+console.log(s);
+var f= 2.13
+console.log(f);
+var x=1;
+var y= 3.14;
+var z=7;
+var add= x + y + z;
+console.log(add)
+var div= x/y;
+console.log(div);
+var mult= x*y*z;
+console.log(mult);
+var remain=x % z;
+var x="cat";
+var y="1";
+var z="dog";
+var q=x + y;
+console.log(q);
+
+
+
+
+
+
+
+
+
+
+
