9d7265a57be40dd25cc06fa74c0afccfb3b68dba
KimberN <kimberly.k.noyes@gmail.com.com>
2021-05-13 14:22:23 -0600

    js grade

--- js/js-basics-2/js-if-statements.js
+++ js/js-basics-2/js-if-statements.js
@@ -1,6 +1,16 @@
 // Define a variable called ‘hoursofSleep’. Set the valueof the variable to 8
-var hoursofSleep = 8;
-if (hoursofSleep <= 6){
-   console.log('I am Groggy')
-}
-else console.log('I feel fantasic!')
+var hoursofSleep = 5;
+if (hoursofSleep <= 6) {
+  console.log("I am Crabby");
+} 
+else console.log("I feel Fantastic");
+
+//exercise 2
+//Write an if statement close that will have the GPS
+//send you a response based on the location
+var nextLocation = "spain";
+if (nextLocation==nextLocation){
+   console.log("It's been a long day, lets go home!");
+}else if(nextLocation== "Work"){
+   console.log("Good morning, finding the fastest route to work!");
+}else console.log("Good morning, finding the fastest route to hmmmm!");
